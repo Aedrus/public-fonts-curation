e3dd7cfbcaeb3a60531d4b1a3b39175fa468c8be
Mario Ferrera <marioferrera10@gmail.com>
2022-11-07 14:08:23 -0500

    Feature: Add Sidebar Functionality
    Add JS file to HTML and functionality to sidebar component.

---
+++ src/js/main.js
@@ -0,0 +1,23 @@
+/* -------- Main JavaScript File.
+ ============================================================================== */
+
+/* =====================================
+   || Sidebar Functionality
+   ===================================== */
+let hamMenu = document.querySelector('.hamburger-menu');
+let sidebar = document.querySelector('.sidebar-container');
+
+
+// element/variable.addEventListener('event', listener receiver());
+
+let toggleSidebar = () => {
+  if (sidebar.style.left === '-800px') {
+    sidebar.style.left = '0px';
+  }
+  else {
+    sidebar.style.left = '-800px'
+  }
+};
+
+hamMenu.addEventListener('click', toggleSidebar);
+
